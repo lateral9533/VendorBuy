604df96c6e4414ad21ab85dc056f9c572aec7724
seshasain <nseshasai5@gmail.com>
2020-10-13 12:08:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vendorbuy/useractivity.java
@@ -0,0 +1,44 @@
+package com.example.vendorbuy;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class useractivity extends AppCompatActivity {
+    Button btnLogOut;
+    FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_useractivity);
+        btnLogOut = (Button) findViewById(R.id.btnLogOut);
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                FirebaseAuth.getInstance().signOut();
+                Intent I = new Intent(useractivity.this, signin.class);
+                startActivity(I);
+
+            }
+        });
+
+    }
+}
