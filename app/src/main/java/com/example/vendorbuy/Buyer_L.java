144d6991822e3c0000f3f0833cea28513e866e16
lateral9533 <lateral9533@gmail.com>
2020-10-14 11:21:42 +0530

    kar gadi laptop 2

---
+++ app/src/main/java/com/example/vendorbuy/Buyer_L.java
@@ -0,0 +1,14 @@
+package com.example.vendorbuy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Buyer_L extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buyer__l);
+    }
+}
