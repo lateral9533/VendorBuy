04f0e30eda7d49d8ce6917cab3e7f92f6ef1b3e4
lateral9533 <lateral9533@gmail.com>
2020-10-14 10:34:15 +0530

    commited

--- app/src/main/java/com/example/vendorbuy/MainActivity.java
+++ app/src/main/java/com/example/vendorbuy/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
             finish();
         }
     }
-    //seshasai
+    //seshasai-lokesh
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
