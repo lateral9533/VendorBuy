0821cf193879738ce73485930ac18094a190aceb
lateral9533 <lateral9533@gmail.com>
2020-10-14 12:35:41 +0530

    food things added

---
+++ app/src/main/java/com/example/vendorbuy/seller_L.java
@@ -0,0 +1,14 @@
+package com.example.vendorbuy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class seller_L extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seller__l);
+    }
+}
